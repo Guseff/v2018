7722cd5246d604d012e727cb8d8e085310467d2d
Guseff <vasily.guseff@gmail.com>
2019-03-09 22:30:01 +0300

    Trade, Analyze and Arbitrage for Desktop added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,94 @@ import './App.css';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ProductTour from './components/ProductTour';
+import OptionBlock from './components/OptionBlock';
+
+const OPTIONS = [
+  {
+    reverse: false,
+    title: 'Trade',
+    text: 'Bitsgap is the easiest way to access all your exchanges at once.',
+    img: require('./assets/images/trade-img-1@2x.png'),
+    list: [
+      {
+        ico: require('./assets/images/coin-ic.svg'),
+        span: 'Switch between 30+ crypto exchanges',
+        text: 'and trade on over 10000 pairs within one simple interface',
+        active: true,
+      },
+      {
+        ico: require('./assets/images/star-ic.svg'),
+        span: 'All your favorite pairs',
+        text: 'with price change at the single glance',
+      },
+      {
+        ico: require('./assets/images/umbrella-ic.svg'),
+        span: 'Place smart orders and control your risks',
+        text: 'with Stop Loss / Take Profit feature NEW',
+      },
+    ],
+  },
+  {
+    reverse: true,
+    title: 'Analyze',
+    text: 'Keep an eye on your portfolio in real-time with always (up-to-date/current/up-to-second) prices.',
+    img: require('./assets/images/analyze-img-1.svg'),
+    list: [
+      {
+        ico: require('./assets/images/magic-ic.svg'),
+        span: 'No more manual trade entry and CSV files - ',
+        text: 'Bitsgap automatically generates reports of your transactions and balance.',
+        active: true,
+      },
+      {
+        ico: require('./assets/images/track-ic.svg'),
+        span: 'Track your Profit/Loss statement',
+        text: 'organized and carefree',
+      },
+      {
+        ico: require('./assets/images/magnifier-ic.svg'),
+        span: 'Get a detailed one-page performance overview',
+        text: 'for all your coins on all exchanges',
+      },
+    ],
+  },
+  {
+    reverse: false,
+    title: 'Arbitrage',
+    text: 'Take advantage of a price difference between two crypto exchanges.',
+    img: require('./assets/images/arbitrage-img-1@2x.png'),
+    list: [
+      {
+        ico: require('./assets/images/cursor-ic.svg'),
+        span: 'Execute complicated orders',
+        text: 'just with one click of the button',
+        active: true,
+      },
+      {
+        ico: require('./assets/images/coins-round-ic.svg'),
+        span: '',
+        text: 'Available for all crypto-to-fiat exchanges',
+      },
+      {
+        ico: require('./assets/images/filter-ic.svg'),
+        span: '',
+        text: 'Smart filters, settings, and history',
+      },
+    ],
+  },
+];
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <Header />
+          <ProductTour />
+          {OPTIONS.map((elem, i) => (
+            <OptionBlock key={i} data={elem} />
+          ))}
           <Footer />
       </div>
     );
---
+++ src/components/OptionBlock/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import ButtonLearn from '../ButtonLearn';
+
+import './style.css';
+
+const OptionBlock = (props) => (
+  <div className={classNames('option', {'option--reverse': props.data.reverse})}>
+    <div className="option-info">
+      <div className="option-title">
+        {props.data.title}
+      </div>
+      <div className="option-text">
+        {props.data.text}
+      </div>
+      <div className="option-list">
+        {props.data.list.map((elem, i) => (
+          <div key={i} className={classNames('option-list-element',
+                            {'option-list-element--active': elem.active}
+                          )}>
+            <img alt='' src={elem.ico} />
+            <span>{elem.span}</span>  {elem.text}
+          </div>
+        ))}
+        </div>
+      <div className="option-button-wrap">
+        <ButtonLearn text='learn more' />
+      </div>
+    </div>
+    <img alt='' src={props.data.img} className={classNames('option-img', 
+                                                  {'option-img--reverse': props.data.reverse}
+                                                )} />
+  </div>
+);
+
+export default OptionBlock; 
